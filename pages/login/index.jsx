272ac4673af9e09fd7b75f7a0bd32e5bcbddf9f4
Nicky Stoyanov <n.stoianov@heatdesign.net>
2022-02-21 21:49:25 +0100

    feat: Create Login Page

---
+++ pages/login/index.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Login from "../../src/components/login/Login";
+
+export default function LoginPage() {
+  return (
+    <Container maxWidth="sm">
+      <Login />
+    </Container>
+  );
+}
