cba9c7126e664d3bd2bee1f20eba2dbc4a1e161d
Nicky Stoyanov <n.stoianov@heatdesign.net>
2022-02-21 22:39:14 +0100

    fix: Create Login Page

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,11 +1,17 @@
 import * as React from "react";
-import Container from "@mui/material/Container";
+import styles from "./LoginPage.module.scss";
+import classNames from "classnames";
+import { Container, Paper } from "@mui/material";
 import Login from "../../src/components/login/Login";
 
 export default function LoginPage() {
   return (
-    <Container maxWidth="sm">
-      <Login />
-    </Container>
+    <div className={classNames(styles['wrapper'])}>
+      <Paper className={classNames(styles['paper'])}>
+        <Container className={classNames(styles['container'])}>
+          <Login />
+        </Container>
+      </Paper>
+    </div>
   );
 }
