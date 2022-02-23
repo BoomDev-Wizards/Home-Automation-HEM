097d844a546e172cb066e78a07fef695f763f736
KoychoKoychev <kgkoychev@gmail.com>
2022-02-23 15:08:16 +0200

    fix: Header component logic rewritten

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,17 @@
 import Example from "../src/components/example/Example";
+import * as React from "react";
+import Header from "../src/components/header/Header";
+import Time from "../src/components/time/Time";
+import User from "../src/components/user/User";
+import Weather from "../src/components/weather/Weather";
 
 export default function Index() {
-  return <Example/>;
+  // return <Example/>;
+  return <Header
+            left={<User name="John Doe" avatar="/images/avatar.png" size={114} hasWelcome={true} headingSize="h1" />}
+            right={<React.Fragment>
+                      <Weather degrees={22} type="cloudy" />
+                      <Time />
+                  </React.Fragment>}
+          />
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,12 +2,12 @@ import styles from "./Header.module.scss";
 import classNames from "classnames";
 import { Paper } from "@mui/material";
 
-export default function Header({left,right=[]}) {
+export default function Header({left,right}) {
     return(
         <div className={classNames(styles["header-wrapper"])}>
             <Paper elevation={3} className={classNames(styles.paper)}>
                 <div className={classNames(styles.left)}>{left}</div>
-                <div className={classNames(styles.right)}>{right.map(el=>el)}</div>
+                <div className={classNames(styles.right)}>{right}</div>
             </Paper>
         </div>
     )
