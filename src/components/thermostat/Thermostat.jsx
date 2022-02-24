788b592a3e9d9fd4c580eeb6b51535cb9a7e290c
KoychoKoychev <kgkoychev@gmail.com>
2022-02-24 13:12:54 +0200

    fix: Thermostat button types amended

--- src/components/thermostat/Thermostat.jsx
+++ src/components/thermostat/Thermostat.jsx
@@ -1,6 +1,6 @@
 import styles from "./Thermostat.module.scss";
 import classNames from "classnames";
-import { CircularProgress } from "@mui/material";
+import { Button, CircularProgress } from "@mui/material";
 import { useState } from "react";
 import { AddCircleOutlined, RemoveCircleOutlined } from "@mui/icons-material";
 import { ComposedChart, Line } from "recharts";
@@ -9,15 +9,15 @@ export default function Thermostat({ data }) {
 
     let [temp, setTemp] = useState(25);
 
-    function incrementTemp(ev){
-        if(temp<50){
-            return setTemp(temp+=1);
+    function incrementTemp(ev) {
+        if (temp < 50) {
+            return setTemp(temp += 1);
         }
     }
 
-    function decrementTemp(ev){
-        if(temp>15){
-            return setTemp(temp-=1);
+    function decrementTemp(ev) {
+        if (temp > 15) {
+            return setTemp(temp -= 1);
         }
     }
 
@@ -27,7 +27,7 @@ export default function Thermostat({ data }) {
                 <div className={classNames(styles["circular-progress"])}>
                     <CircularProgress
                         variant="determinate"
-                        value={1.42857*temp-21.42857}
+                        value={1.42857 * temp - 21.42857}
                         size={435}
                         className={classNames(styles.progress)}
                     />
@@ -39,18 +39,22 @@ export default function Thermostat({ data }) {
                     />
                 </div>
                 <div className={classNames(styles["circular-controls"])}>
-                    <RemoveCircleOutlined  onClick={decrementTemp}/>
+                    <Button variant="contained" onClick={decrementTemp}>
+                        <RemoveCircleOutlined />
+                    </Button>
                     <span className={classNames(styles.temp__num)}>{temp}°</span>
-                    <AddCircleOutlined  onClick={incrementTemp} />
+                    <Button variant="contained" onClick={incrementTemp}>
+                        <AddCircleOutlined/>
+                    </Button>
                 </div>
             </div>
             <div className={classNames(styles["linear-progress"])}>
                 <span>Temperature graph</span>
                 <ComposedChart width={800} height={150} data={data}>
-                    <Line 
-                    dot={{ stroke: 'white', strokeWidth: 3, r: 7, fill:'#E150A6' }} 
-                    type='natural' strokeWidth={4} stroke="white"
-                    dataKey='temperature'
+                    <Line
+                        dot={{ stroke: 'white', strokeWidth: 3, r: 7, fill: '#E150A6' }}
+                        type='natural' strokeWidth={4} stroke="white"
+                        dataKey='temperature'
                     />
                 </ComposedChart>
             </div>
