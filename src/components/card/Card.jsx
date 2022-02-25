429c5eabbec5c5408c453bde5918e56fbd05a82d
KoychoKoychev <kgkoychev@gmail.com>
2022-02-25 19:32:59 +0200

    fix: add onClick prop to the Card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,10 +2,10 @@ import styles from "./Card.module.scss";
 import classNames from "classnames";
 import { Button, CardContent, Card as CardComp } from "@mui/material";
 
-export default function Card({ iconUrl, outlined = false }) {
+export default function Card({ iconUrl, outlined = false, onClick }) {
     return (
         <div className={classNames(styles.card, outlined?styles.outlined:null)}>
-            <Button variant="text" className={classNames(styles.button)}>
+            <Button variant="text" className={classNames(styles.button)} onClick={onClick}>
                 <CardComp className={classNames(styles.wrapper)}>
                     <CardContent>
                         {iconUrl
