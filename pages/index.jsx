fde177322e9700f98817510d777020fc6318c053
Nicky Stoyanov <n.stoianov@heatdesign.net>
2022-02-25 19:49:08 +0100

    feat: Create Scenes Component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,29 @@
-import * as React from "react";
-import Card from "../src/components/card/Card";
+import { React, useState, useEffect, Fragment } from "react";
 import Header from "../src/components/header/Header";
 import Navigation from "../src/components/navigation/Navigation";
+import Scenes from "../src/components/scenes/Scenes";
 import Time from "../src/components/time/Time";
 import User from "../src/components/user/User";
 import Weather from "../src/components/weather/Weather";
+import devicesData from "../data/devices.json";
 
 export default function Index() {
+  const [devices, setDevices] = useState([]);
+  useEffect(() => {
+    setDevices(devicesData);
+  }, []);
 
   return (
-    <React.Fragment>
+    <Fragment>
       <Header
         left={<User name="John Doe" avatar="/images/avatar.png" size={114} hasWelcome={true} headingSize="h1" />}
-        right={<React.Fragment>
+        right={<Fragment>
           <Weather degrees={22} type="cloudy" />
           <Time />
-        </React.Fragment>}
+        </Fragment>}
       />
-      <Card iconUrl={'/images/alarm-clock.svg'} outlined={true}/>
+      <Scenes cards={devices} />
       {/* <Navigation /> */}
-    </React.Fragment>
+    </Fragment>
   )
 }
---
+++ src/components/scenes/Scenes.jsx
@@ -0,0 +1,18 @@
+import styles from "./Scenes.module.scss";
+import classNames from "classnames";
+import { Grid } from "@mui/material";
+import Card from "../../components/card/Card";
+
+export default function Scenes({ cards, selected }) {
+    return (
+        <div className={classNames(styles["scenes-container"])}>
+            <Grid container spacing={2}>
+                {cards.length !== 0 && cards.devices.map((card, key) => (
+                    <Grid item key={key} xs={4}>
+                        <Card iconUrl={card.iconUrl} />
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
+    )
+}
