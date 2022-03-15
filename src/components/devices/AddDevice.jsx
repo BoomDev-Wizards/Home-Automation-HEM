77ef50321eef5ad2c6d48fda19a59f537443b899
KoychoKoychev <kgkoychev@gmail.com>
2022-03-15 13:46:32 +0200

    feat: change the displayed text when the searching has failed

--- src/components/devices/AddDevice.jsx
+++ src/components/devices/AddDevice.jsx
@@ -60,7 +60,7 @@ export default function AddDevice({ searching, found, failed, onDevice, open, ha
                             variant="h6"
                             className={classNames(styles["action_text"])}
                         >
-                            NO DEVICES FOUND...
+                            NO DEVICE FOUND
                         </Typography>
                     </div>
                     : onDevice ?
