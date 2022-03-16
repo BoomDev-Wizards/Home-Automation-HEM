5a7b6d24611833e554fa34bed2e469bcdcfab21d
KoychoKoychev <kgkoychev@gmail.com>
2022-03-16 20:43:18 +0200

    feat: implement register functionality

--- pages/register/index.jsx
+++ pages/register/index.jsx
@@ -1,15 +1,37 @@
-import * as React from "react";
 import styles from "./RegisterPage.module.scss";
 import classNames from "classnames";
 import { Container, Paper } from "@mui/material";
 import Register from "../../src/components/register/Register";
+import { useState } from "react";
+import { useRouter } from "next/dist/client/router";
 
 export default function RegisterPage() {
+
+  const router = useRouter();
+
+  const [errors,setErrors] = useState({})
+
+  async function onSubmit(userData) {
+      const result = await fetch('https://hem-api.herokuapp.com/register',{
+        method: 'POST',
+        headers:{
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData)
+      });
+      const data = await result.json();
+      if(result.status!==200 && result.status!==201){
+        setErrors(data);
+      }else{
+        router.push('/')
+      }
+  }
+
   return (
     <div className={classNames(styles["wrapper"])}>
       <Paper className={classNames(styles["paper"])}>
         <Container className={classNames(styles["container"])}>
-          <Register />
+          <Register onSubmit={onSubmit} err={errors} />
         </Container>
       </Paper>
           <p>Already registered? <a href="/login">Go to login.</a></p>
