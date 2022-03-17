7c8e8c6e1b1e5f25e3a445e9e0b72ce508fff82f
KoychoKoychev <kgkoychev@gmail.com>
2022-03-17 14:02:16 +0200

    fix: move the AppProvider component to a different location

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,7 +6,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
-import AppProvider from '../src/components/context/AppProvider';
+import AppProvider from '../src/components/common/AppProvider';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
